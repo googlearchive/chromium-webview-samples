d94418a849dec8aebac7832252e3b4fcc91d4bba
Matt Gaunt <matt@gauntface.co.uk>
2014-10-17 17:51:28 +0100

    Switching to simpl.info video

---
+++ web-touch-example/app/src/main/java/webtouch/example/android/chrome/google/com/webtouch/MainActivity.java
@@ -0,0 +1,63 @@
+package webtouch.example.android.chrome.google.com.webtouch;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+
+@SuppressLint("SetJavaScriptEnabled")
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get reference of WebView from layout/activity_main.xml
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        setUpWebViewDefaults(mWebView);
+
+        mWebView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    /**
+     * Convenience method to set some generic defaults for a
+     * given WebView
+     *
+     * @param webView
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setUpWebViewDefaults(WebView webView) {
+        WebSettings settings = webView.getSettings();
+
+        // Enable Javascript
+        settings.setJavaScriptEnabled(true);
+
+        // Use WideViewport and Zoom out if there is no viewport defined
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+
+        // Enable pinch to zoom without the zoom buttons
+        settings.setBuiltInZoomControls(true);
+
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+            // Hide the zoom controls for HONEYCOMB+
+            settings.setDisplayZoomControls(false);
+        }
+
+        // Enable remote debugging via chrome://inspect
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+    }
+
+}
